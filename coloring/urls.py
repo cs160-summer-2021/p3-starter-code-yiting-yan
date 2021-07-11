280ad3a02d7e3e7e6e3b697da496c7a94f1e4680
ML <70348681+mxl1n@users.noreply.github.com>
2021-07-11 02:10:29 -0400

    kind of implemented subcolors

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.new_interaction, name='new_interaction')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
