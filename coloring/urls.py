ce8b09c777da85dc26d7110767e46a029837a72a
yiting-yan <tinayan1122@gmail.com>
2021-07-11 23:20:02 +0800

    fix clicking error

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction'),
     path('palette', views.palette, name='palette'),
+    path('palette2', views.palette2, name='palette2'),
     path('new_interaction', views.new_interaction, name='new_interaction')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'coloring/index.html')
 
+
 def palette(request):
     return render(request, 'coloring/palette.html')
+
+
+def palette2(request):
+    return render(request, 'coloring/palette2.html')
+
+
 def new_interaction(request):
     return render(request, 'coloring/new_interaction.html')
