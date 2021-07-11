873e50ac6bdebc6a97629f7c940649803ba667e7
yiting-yan <tinayan1122@gmail.com>
2021-07-11 15:52:03 +0800

    add path to color palette page

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('palette', views.palette, name='palette')
 ]
